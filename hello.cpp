cc2628fe3bcd72aea412fdae0b239680e4e63677
vivekraj2528 <vivek4478raj@gmail.com>
2025-07-30 12:50:58 +0530

    hii

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
     int main()
     {
         int a;
-        cout<<"enter number";
+        cout<<"enter the number";
         cin>>a;
         if (a<20) cout<<"fall in age criteria";
         return 0;
